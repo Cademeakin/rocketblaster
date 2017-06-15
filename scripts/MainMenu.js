1dca0dd826333c30e9d5a9c359be0805a1248885
Cade Meakin <lazygamers98@gmail.com>
2017-06-15 09:58:11 +0100

    MainMenu Step 1
    MainMenu Step 1

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -3,11 +3,28 @@ BasicGame.MainMenu = function (game) { };
 BasicGame.MainMenu.prototype = {
 
 	create: function () {
+        
+        // We've already preloaded our assets, so let's get into the Main Menu itself.
+        // Here we are playing music and adding a pic and button.
+        // Outputting sky, ship, score, lives, total and Start Time to the screen.
+        // The scrolling starfield background
+        starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
+        logo = this.add.sprite((this.world.width / 2), (this.world.height / 2) - 150, 'logo');
+        logo.anchor.setTo(0.5,0.5);
+        startButton = this.add.button((this.world.width / 2),(this.world.height / 2) + 50, 'startButton', this.startGame);
+        startButton.anchor.setTo(0.5,0.5);
+                                      
 
 	},
 
 	update: function () {
 		//	Do some nice funky main menu effect here
-	}
+	},
+    
+    startGame: function () {
+        //Start the actual game
+        this.game.state.start('Game');
+        
+    }
 
 };
