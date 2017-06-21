483f6d75a8b2bda02ef2b0c0dfad5a95a7f2ff9d
Cade Meakin <lazygamers98@gmail.com>
2017-06-21 12:20:16 +0100

    revert

--- scripts/Preloader.js
+++ scripts/Preloader.js
@@ -7,20 +7,22 @@ BasicGame.Preloader = function (game) {
 BasicGame.Preloader.prototype = {
 
 	preload: function () {
-        //Displays a loading screen message while the asset are loaded into memory
-				this.preloaderText = this.add.text(this.world.centerX, this.world.centerY, 'Loading...',
-			{
-				fontSize: '96px',
+        //Displays a loading screen message while the assets are loaded into memory
+				this.preloaderText = this.add.text(this.world.centerX, this.world.centerY, 'Loading...', {
+				fontsize: '96px',
 				fill: '#fff',
 				align: 'center'
 			});
 			this.preloaderText.anchor.setTo(0.5,0.5);
 
-			//preload the images, sprites and audio assets into memory
+			//preload the images, sprites and audio into memory
 			this.load.image('logo', 'assets/PhaserLogo.png');
 			this.load.image('starfield', 'assets/starfield.png');
 			this.load.image('startButton', 'assets/startButton.png');
 			this.load.image('ship', 'assets/ship.png');
+            this.load.image('ufo', 'assets/ufo.png');
+            this.load.image('life', 'assets/lives.png');
+            this.load.image('bullet', 'assets/bullet.png');
 	},
 
 	create: function () {
@@ -28,7 +30,9 @@ BasicGame.Preloader.prototype = {
 	},
 
 	update: function () {
-			this.game.state.start('MainMenu');
+        
+        // You don't actually need to do this, but I find it gives a much smoother game experience. Basically it will wait for our audio file to be decoded before proceeding to the MainMenu. You can jump right into the menu if you want and still play the music, but you'll have a few seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu it's best to wait for it to decode here first, then carry on. If you don't have any music in your game then put the game.state.start line into the create function and delete the update function completely.
+		this.game.state.start('MainMenu');
 	}
 
 };
