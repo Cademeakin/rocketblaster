ac4cacb40ac7dd2bd38cd476acaacf0fc5fe1ee9
Cade Meakin <lazygamers98@gmail.com>
2017-06-21 12:08:36 +0100

    Test

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -1,34 +1,32 @@
 BasicGame.MainMenu = function (game) { };
 
-    var startButton;
-    var starfield;
-    var logo;
+	var startButton;
+	var starfield;
+	var logo;
 
 BasicGame.MainMenu.prototype = {
 
 	create: function () {
-        
-        // We've already preloaded our assets, so let's get into the Main Menu itself.
-        // Here we are playing music and adding a pic and button.
-        // Outputting sky, ship, score, lives, total and Start Time to the screen.
-        // The scrolling starfield background
-        starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
-        logo = this.add.sprite((this.world.width / 2), (this.world.height / 2) - 150, 'logo');
-        logo.anchor.setTo(0.5,0.5);
-        startButton = this.add.button((this.world.width / 2), (this.world.height / 2) + 50, 'startButton', this.startGame);
-        startButton.anchor.setTo(0.5,0.5);
-                                      
+		//We've already loaded the assets so we'll move straight into the MainMenu
+		//Here all we are doing is playing music, adding a picture and a button
+		//You will modify the MainMenu to suit your game
 
+		//Output sky, ship, score, lives, total and Start Time to the screen
+		//The scrolling starfield background
+		starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
+		logo = this.add.sprite((this.world.width / 2), (this.world.height / 2) - 150, 'logo');
+		logo.anchor.setTo(0.5,0.5);
+		startButton = this.add.button((this.world.width / 2), (this.world.height / 2) + 50, 'startButton', this.startGame);
+		startButton.anchor.setTo(0.5,0.5);
 	},
 
 	update: function () {
 		//	Do some nice funky main menu effect here
 	},
-    
-    startGame: function () {
-        //Start the actual game
-        this.game.state.start('game');
-        
-    }
+
+	startGame: function () {
+		//And start the actual game
+		this.game.state.start('Game');
+	}
 
 };
