4fc92d2f8981f437d69ace77608238ca3aa569e2
Cade Meakin <lazygamers98@gmail.com>
2017-06-21 11:22:05 +0100

    Revert

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -1,3 +1,4 @@
+var BasicGame = {};
 BasicGame.Game = function (game) {};
 
 //Graphical Objects
@@ -8,6 +9,17 @@ var lives; //Group of Lives which are collected
 var bullets; //Bullets which your spaceship fires
 var fireRate = 100; // Rate at which bullets are fired
 var nextFire = 0;
+
+//Score & Life Objects
+var score; //Players Score
+var lifeTotal; //Players total number of lives
+var scoreText; //Text which is used to display the score
+var lifeTotalText; //Text which is used to display the number of lives
+
+//Timer Variables stores information about the timer
+var seconds; //Number of seconds game has been running
+var timer;
+var timerText;
 
 //Misc Variables
 var cursors; //Keyboard control
@@ -40,6 +52,7 @@ BasicGame.Game.prototype = {
         
             lives = this.add.group();
             this.physics.enable(lives, Phaser.Physics.ARCADE);
+        
             lives.setAll('outOfBoundsKill', true);
             lives.setAll('checkWorldBounds', true);
             lives.setAll('anchor.x', 0.5);
@@ -53,10 +66,41 @@ BasicGame.Game.prototype = {
 			 bullets.setAll('anchor.y', 0.5);
 			 bullets.setAll('outOfBoundsKill', true);
 			 bullets.setAll('checkWorldBounds', true);
+        
+        //Setting up and adding the Score, Life and Timer to the Screen
+        scoreText = this.add.text(16, 16, 'Score: 0', {
+            font: '32px arial',
+            fill: '#fff'
+            });
+        
+        //sets the score to 0 and output to the screen
+        score = 0;
+        scoreText.text = "Score: " + score;
+        lifeTotalText = this.add.text(this.world.width - 150, 16, 'Lives:3', {
+            font: '32px arial',
+            fill: '#fff'
+            });
+        
+        //sets the lifeTotal to 3 and output to the screen
+        lifeTotal = 3;
+        lifeTotalText.text = 'Lives: ' + lifeTotal;
+        timerText = this.add.text(350, 16, 'Time: 0', {
+            font: '32px arial',
+            fill: '#fff'
+            });
+        
+        //setup timer
+        timer = this.time.create(false);
+        seconds = 0;
+        timerText.text = 'Time: ' + seconds;
 
 			 //Setting the keyboard to accept LEFT, RIGHT and SPACE input
 			 this.input.keyboard.addKeyCapture([Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR]);
 			 cursors = this.input.keyboard.createCursorKeys();
+        
+            //Set a TimerEvent to occur every second and start the timer
+            timer.loop(1000, this.updateTimer, this);
+            timer.start();
 	},
 
 	update: function () {
@@ -64,6 +108,7 @@ BasicGame.Game.prototype = {
             this.createUfo();
             this.createLife();
 			this.moveShip();
+            this.collisionDetection();
 			},
 
 		//moves ship and fires bullet from keyboard controls
@@ -102,7 +147,7 @@ BasicGame.Game.prototype = {
             this.physics.enable(ufo, Phaser.Physics.ARCADE);
             
             //Generating a random velocity
-            ufo.body.velocity.y = this.rnd.integerInRange(100, 600);
+            ufo.body.velocity.y = this.rnd.integerInRange(200, 300);
             }
         },
     
@@ -131,5 +176,34 @@ BasicGame.Game.prototype = {
 					bullet.reset(ship.x, ship.y);
 					bullet.body.velocity.y = -400;
 					}
-				}
-			};
+				},
+        //function executed during playing the game to check for collisions
+    collisionDetection: function () {
+        this.physics.arcade.overlap(ship, ufos, this.collideUfo, null, this);
+        this.physics.arcade.overlap(ship, lives, this.collectLife, null, this);
+        this.physics.arcade.overlap(bullets, ufos, this.destroyUfo, null, this);
+        },
+            
+            //function executed if there is collision between player and ufo. UFO is destroyed, animation & sound, reduce lifeTotal
+            collideUfo: function (ship,ufo) {
+                ufo.kill();
+                },
+                    //function executed if there is collision between ufo and bullet. UFO is destroyed, animation & sound, increase score
+                    destroyUfo: function (bullet, ufo) {
+                        ufo.kill();
+                        bullet.kill();
+                        },
+                    //function executed if there is collision between player and life. Life is destroyed, animation & sound, increase lifeTotal
+                            collectLife: function (ship, life) {
+                                life.kill();
+                                },
+                        
+                    //Updates timer and outputs to the screen
+                                    updateTimer: function () {
+                                        seconds++;
+                                        timerText.text = 'Time: ' + seconds;
+                                        }
+};
+
+    
+
