89172c85eff85dd9cdeea3aaf9e70803168be369
Cade Meakin <lazygamers98@gmail.com>
2017-06-24 02:10:26 +0100

    Update MainGame.js

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -278,10 +278,6 @@ BasicGame.Game.prototype = {
             this.game.state.start('Game');
             },
     
-    render: function() {
-    // Sprite debug info
-    this.game.debug.bodyInfo(ship, 32, 100);
-    this.game.debug.spriteBounds(ship);
-}
+  
 
 };
